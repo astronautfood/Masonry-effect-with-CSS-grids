ce2347bb75e8928582c7b06a9357dffb75e93376
Dana Manoli <dmanoli1@gmail.com>
2017-10-07 14:04:47 -0400

    update

--- dev/scripts/script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	
-	var $hamburger = $(".hamburger");
-		$hamburger.on("click", function(e) {
-			$('header nav').toggleClass('overlay');
-			$('nav .nav__items').toggleClass('show');
-	});
-});
